b7d1140535b989a7e3b2cdf3a735094fcaa06732
Dario Tobar <dtp0006@alu.ubu.es>
2016-06-25 18:38:34 +0200

    Extract method para eliminar duplicacidad
    Creación de método privado typeNode para eliminar la duplicidad del
    witch case de tipo de nodo.

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -369,26 +369,7 @@ Write a HTML representation of #receiver on the given #buf.
 		buf.append("\n\n<UL>");
 		do {
 			buf.append("\n\t<LI> ");
-			switch (currentNode.type_) {
-			case Node.NODE:
-				buf.append("Node ");
-				buf.append(currentNode.name_);
-				buf.append(" [Node]");
-				break;
-			case Node.WORKSTATION:
-				buf.append("Workstation ");
-				buf.append(currentNode.name_);
-				buf.append(" [Workstation]");
-				break;
-			case Node.PRINTER:
-				buf.append("Printer ");
-				buf.append(currentNode.name_);
-				buf.append(" [Printer]");
-				break;
-			default:
-				buf.append("(Unexpected)");;
-				break;
-			};
+			typeNode(currentNode,buf);
 			buf.append(" </LI>");
 			currentNode = currentNode.nextNode_;
 		} while (currentNode != firstNode_);
@@ -406,29 +387,34 @@ Write an XML representation of #receiver on the given #buf.
 		buf.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n<network>");
 		do {
 			buf.append("\n\t");
-			switch (currentNode.type_) {
-			case Node.NODE:
-				buf.append("<node>");
-				buf.append(currentNode.name_);
-				buf.append("</node>");
-				break;
-			case Node.WORKSTATION:
-				buf.append("<workstation>");
-				buf.append(currentNode.name_);
-				buf.append("</workstation>");
-				break;
-			case Node.PRINTER:
-				buf.append("<printer>");
-				buf.append(currentNode.name_);
-				buf.append("</printer>");
-				break;
-			default:
-				buf.append("<unknown></unknown>");;
-				break;
-			};
+			typeNode(currentNode,buf);
 			currentNode = currentNode.nextNode_;
 		} while (currentNode != firstNode_);
 		buf.append("\n</network>");
+	}
+	
+	private void typeNode(Node currentNode, StringBuffer buf){
+		switch (currentNode.type_) {
+		case Node.NODE:
+			buf.append("<node>");
+			buf.append(currentNode.name_);
+			buf.append("</node>");
+			break;
+		case Node.WORKSTATION:
+			buf.append("<workstation>");
+			buf.append(currentNode.name_);
+			buf.append("</workstation>");
+			break;
+		case Node.PRINTER:
+			buf.append("<printer>");
+			buf.append(currentNode.name_);
+			buf.append("</printer>");
+			break;
+		default:
+			buf.append("<unknown></unknown>");;
+			break;
+		};
+	
 	}
 
 }
