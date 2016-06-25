ce649202f3dec25aa5a2fb130807dcbfabb0400b
Dario Tobar <dtp0006@alu.ubu.es>
2016-06-25 17:07:25 +0200

    Extract method de printDocument
    Creación de printAccount() para eliminar la duplicidad de código.

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -263,6 +263,16 @@ Therefore #receiver sends a packet across the token ring network, until either
 
 		return result;
 	}
+	
+	private void printAccount(Writer report, String author, String title) throws IOException {
+		report.write("\tAccounting -- author = '");
+		report.write(author);
+		report.write("' -- title = '");
+		report.write(title);
+		report.write("'\n");
+		report.write(">>> ASCII Print job delivered.\n\n");
+		report.flush();
+	}
 
 	private boolean printDocument (Node printer, Packet document, Writer report) {
 		String author = "Unknown";
@@ -282,24 +292,12 @@ Therefore #receiver sends a packet across the token ring network, until either
 							endPos = document.message_.indexOf(".", startPos + 6);
 							if (endPos < 0) {endPos = document.message_.length();};
 							title = document.message_.substring(startPos + 6, endPos);};
-							report.write("\tAccounting -- author = '");
-							report.write(author);
-							report.write("' -- title = '");
-							report.write(title);
-							report.write("'\n");
-							report.write(">>> Postscript job delivered.\n\n");
-							report.flush();
+							printAccount(report, author, title);
 				} else {
 					title = "ASCII DOCUMENT";
 					if (document.message_.length() >= 16) {
 						author = document.message_.substring(8, 16);};
-						report.write("\tAccounting -- author = '");
-						report.write(author);
-						report.write("' -- title = '");
-						report.write(title);
-						report.write("'\n");
-						report.write(">>> ASCII Print job delivered.\n\n");
-						report.flush();
+						printAccount(report, author, title);
 				};
 			} catch (IOException exc) {
 				// just ignore
