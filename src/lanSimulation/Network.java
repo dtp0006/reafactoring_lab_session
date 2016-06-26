484de69c8f4841809f3d44a820e31e50f5d3d65d
Dario Tobar <dtp0006@alu.ubu.es>
2016-06-26 17:34:56 +0200

    #3 Extract switch PrintXML

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -322,26 +322,7 @@ Write an XML representation of #receiver on the given #buf.
 		buf.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n<network>");
 		do {
 			buf.append("\n\t");
-			switch (currentNode.type_) {
-			case Node.NODE:
-				buf.append("<node>");
-				buf.append(currentNode.name_);
-				buf.append("</node>");
-				break;
-			case Node.WORKSTATION:
-				buf.append("<workstation>");
-				buf.append(currentNode.name_);
-				buf.append("</workstation>");
-				break;
-			case Node.PRINTER:
-				buf.append("<printer>");
-				buf.append(currentNode.name_);
-				buf.append("</printer>");
-				break;
-			default:
-				buf.append("<unknown></unknown>");;
-				break;
-			};
+			typeNodeXML(currentNode,buf);
 			currentNode = currentNode.nextNode_;
 		} while (currentNode != firstNode_);
 		buf.append("\n</network>");
@@ -366,6 +347,29 @@ Write an XML representation of #receiver on the given #buf.
 			break;
 		default:
 			buf.append("(Unexpected)");;
+			break;
+		};
+	}
+	
+	private void typeNodeXML(Node currentNode, StringBuffer buf){
+		switch (currentNode.type_) {
+		case Node.NODE:
+			buf.append("<node>");
+			buf.append(currentNode.name_);
+			buf.append("</node>");
+			break;
+		case Node.WORKSTATION:
+			buf.append("<workstation>");
+			buf.append(currentNode.name_);
+			buf.append("</workstation>");
+			break;
+		case Node.PRINTER:
+			buf.append("<printer>");
+			buf.append(currentNode.name_);
+			buf.append("</printer>");
+			break;
+		default:
+			buf.append("<unknown></unknown>");;
 			break;
 		};
 	}
